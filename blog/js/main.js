0811f5d2ff907a1bc18e197edfa3fb8303f3a8e5
Caglar Gonul <caglar.gonul.001@gmail.com>
2014-10-20 15:03:08 +0300

    Home page, footer and header files are added.

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,1 +1,25 @@
+var app = angular.module('cgonul_static_web_site', ['ngRoute']);
 
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        // Home
+        .when("/", {templateUrl: "pages/home.html", controller: "PageCtrl"});
+}]);
+
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+    console.log("Page Controller reporting for duty.");
+
+    // Activates the Carousel
+    $('.carousel').carousel({
+        interval: 5000
+    });
+
+    // Activates Tooltips for Social Links
+    $('.tooltip-social').tooltip({
+        selector: "a[data-toggle=tooltip]"
+    })
+});
